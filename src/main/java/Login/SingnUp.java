eb454fbdae86427d9bfeaa4413afc682dbe2c533
ShyikaAnna <kanvalyariya@gmail.com>
2018-04-26 11:49:26 +0300

    Sign up updating

--- src/main/java/Login/SingnUp.java
+++ src/main/java/Login/SingnUp.java
@@ -1,7 +1,24 @@
 package Login;
 
-/**
- * Created by Anna on 25.04.2018.
- */
-public class SingnUp {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+
+public class SingnUp extends TestBase{
+    private String singUpForm = "/html/body/div[1]/div/div[1]/div[2]/form/div[2]";
+    private String fullname = "mui-id-0";
+    @Test
+    public void singnUp() {
+        driver.navigate().to("https://todo.vu");
+        driver.findElement(By.linkText("SIGN UP")).click();
+        By lsingUpForm = By.xpath(singUpForm);
+        WebElement form = driver.findElement(lsingUpForm);
+        wait.until(ExpectedConditions.visibilityOf(form));
+    }
+    public void fillUpFullname(){
+        By lfullname = By.id(fullname);
+        driver.findElement(lfullname).sendKeys("Gregory Alvarez");
+    }
 }
--- src/main/java/Login/TestBase.java
+++ src/main/java/Login/TestBase.java
@@ -2,11 +2,8 @@ package Login;
 
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -20,23 +17,9 @@ public class TestBase {
 
     @Before
     public void start() {
-      /*  if (tlDriver.get() != null) {
-            driver = tlDriver.get();
-            wait = new WebDriverWait(driver, 10);
-            return;
-        }
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(FirefoxDriver.MARIONETTE, false);
-        driver = new FirefoxDriver(caps);
-        tlDriver.set(driver);
-        System.out.println(((HasCapabilities) driver).getCapabilities());
-        wait = new WebDriverWait(driver, 10);*/
-
-        /*Runtime.getRuntime().addShutdownHook(
-                new Thread(() -> { driver.quit(); driver = null; }));*/
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().deleteAllCookies();
         ((RemoteWebDriver) driver).setLogLevel(Level.FINEST);
         driver.manage().window().maximize();
